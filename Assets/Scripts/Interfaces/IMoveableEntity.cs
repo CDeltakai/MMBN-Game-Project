89f1475a46c5847ad67cf872a16a2c054c6c9728
CDeltakai <deltakai2010@gmail.com>
2022-07-28 14:49:55 +1000

    added interface for Movevable entities

---
+++ Assets/Scripts/Interfaces/IMoveableEntity.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMoveableEntity
+{
+
+    public void checkValidTile(Vector3Int pos);
+    
+
+
+
+}
