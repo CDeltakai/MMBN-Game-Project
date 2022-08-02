76ce6df08d9e5e9169476c1fef92dfbe4d5271c5
CDeltakai <deltakai2010@gmail.com>
2022-08-02 14:58:30 +1000

    Added test code for instantiating Longsword vfx

--- Assets/Scripts/ChipEffectScripts/Longsword.cs
+++ Assets/Scripts/ChipEffectScripts/Longsword.cs
@@ -6,6 +6,7 @@ using UnityEngine.AddressableAssets;
 public class Longsword : MonoBehaviour, IChip
 {
     public Transform firePoint;
+    BattleStageHandler stageHandler;
     public int BaseDamage {get;set;} = 80;
 
     public int AdditionalDamage{get; set;} = 0;
@@ -15,9 +16,14 @@ public class Longsword : MonoBehaviour, IChip
     public EChipElements chipElement => EChipElements.Blade;
 
     PlayerMovement player;
+    public Vector3 initPosition;
 
     void Start()
     {
+        player = FindObjectOfType<PlayerMovement>();
+        stageHandler = FindObjectOfType<BattleStageHandler>();
+        initPosition.Set(player.transform.localPosition.x + 1.6f, player.transform.localPosition.y, 0);
+        firePoint.localPosition = initPosition;
 
     }
 
@@ -25,7 +31,7 @@ public class Longsword : MonoBehaviour, IChip
     public void Effect(int AddDamage = 0, EStatusEffects statusEffect = EStatusEffects.Default)
     {
 
-
+        Addressables.InstantiateAsync("VFX_Longsword_pos", firePoint);
     }
 
     
