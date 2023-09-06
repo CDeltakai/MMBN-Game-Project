d0055056de7a5ed329d2d444658a8d5cc7bf16ea
Matthew Qiu <deltakai2010@gmail.com>
2023-09-06 18:17:09 +1000

    refactoring deck system etc.

--- Assets/Scripts/UIScripts/CardObjectReference.cs
+++ Assets/Scripts/UIScripts/CardObjectReference.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
@@ -7,7 +8,10 @@ public class CardObjectReference
 {
     public ChipSO chipSO;
     public GameObject effectPrefab;
-    public List<CardObjectReference> attachedPassiveCards = new List<CardObjectReference>();
+    [SerializeField] CardObjectReference passiveCard1;
+    [SerializeField] CardObjectReference passiveCard2;
+    [SerializeField] CardObjectReference passiveCard3;
+    [SerializeField] CardObjectReference passiveCard4;
     public GameObject ObjectSummon;
     public List<GameObject> ObjectSummonList;
     public CardSlot cardSlot;
@@ -16,14 +20,24 @@ public class CardObjectReference
     {
         chipSO = null;
         effectPrefab = null;
-        attachedPassiveCards.Clear();
+        
+        passiveCard1 = null;
+        passiveCard2 = null;
+        passiveCard3 = null;
+        passiveCard4 = null;        
+
+
         ObjectSummon = null;
         ObjectSummonList.Clear();
     }
 
     public void ClearPassiveCards()
     {
-        attachedPassiveCards.Clear();
+        passiveCard1 = null;
+        passiveCard2 = null;
+        passiveCard3 = null;
+        passiveCard4 = null;   
+
     }
 
     public bool IsEmpty()
@@ -33,6 +47,36 @@ public class CardObjectReference
             return true;
         }
         return false;
+    }
+
+    public void AddCardToPassives(CardObjectReference card)
+    {
+        if(passiveCard1 == null || passiveCard1.IsEmpty())
+        {
+            passiveCard1 = card;
+        }else
+        if(passiveCard2 == null || passiveCard2.IsEmpty())
+        {
+            passiveCard2 = card;
+        }else
+        if(passiveCard3 == null || passiveCard3.IsEmpty())
+        {
+            passiveCard3 = card;
+        }else
+        if(passiveCard4 == null || passiveCard4.IsEmpty())
+        {
+            passiveCard4 = card;
+        }
+
+    }
+
+    public List<CardObjectReference> GetPassiveCards()
+    {
+        List<CardObjectReference> passiveCards = new List<CardObjectReference>
+        {
+            passiveCard1
+        };
+        return passiveCards;
     }
 
 }
